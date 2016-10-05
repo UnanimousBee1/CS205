25c67e1efab1917e8e1c197a3e96e0d51c43c59e
UnanimousBee1 <siddhant.bhargava_ug18@ashoka.edu.in>
2016-10-05 22:29:11 +0530

    All tree answers

---
+++ Practice-Questions-2/Q8.scala
@@ -0,0 +1,86 @@
+  class Tree();
+
+ case class EmptyTree() extends Tree;
+ 
+ case class NodeTree(data: Int, left:Tree, right:Tree) extends Tree;
+
+
+object Q8 {
+   def main(args: Array[String]): Unit = {
+     
+     var newTree = new Tree
+     
+     //-------------------------------------------------------------------------------------------------------
+     def stringify(tree: Tree): String = {
+       tree match{
+         case EmptyTree() => "";
+         case NodeTree(x:Int, y:Tree, z:Tree) => stringify(y) +" "+ x.toString() + " " +stringify (z);
+         
+       }
+     }
+     //-------------------------------------------------------------------------------------------------------
+      def InsTree(tree: Tree,n: Int): Tree = {
+        tree match{
+          case EmptyTree() => NodeTree(n, EmptyTree(), EmptyTree())
+          case NodeTree(x:Int, y: Tree, z:Tree) => if(n<=x)  NodeTree(x:Int, InsTree(y,n), z:Tree) 
+                                                   else      NodeTree(x:Int, y, InsTree(z,n))
+        }
+      }
+     
+     
+     //-------------------------------------------------------------------------------------------------------
+     def doubleEle(tree: Tree ): Tree = {
+       tree match{
+         case EmptyTree() => EmptyTree();
+         case NodeTree(x:Int, y:Tree, z:Tree) => NodeTree(x*2, doubleEle(y), doubleEle(z));
+         
+       }
+
+     }
+     //-------------------------------------------------------------------------------------------------------
+     def MapTree(tree: Tree,f: Int => Int): Tree = {
+       tree match{
+         case EmptyTree() => EmptyTree();
+         case NodeTree(x:Int, y:Tree, z:Tree) => NodeTree (f(x), MapTree(y,f), MapTree(z,f))
+       }
+       
+     }
+     //-------------------------------------------------------------------------------------------------------     
+     def Sum(tree: Tree): Int = {
+       tree match{
+         case EmptyTree() => 0
+         case NodeTree(x:Int, y:Tree, z:Tree) => x+Sum(y)+Sum(z)      
+       }   
+     }
+     //-------------------------------------------------------------------------------------------------------     
+     def FoldTree(n: Int, tree: Tree, f: (Int , Int) => Int): Int = {
+       tree match{
+         case EmptyTree() => n
+         case NodeTree(x:Int, y:Tree, z:Tree) => FoldTree(FoldTree(f(x,n),y,f),z,f)
+       }
+     }
+     //-------------------------------------------------------------------------------------------------------     
+     def cont(lst1:List[Int],lst2:List[Int],count:Int): Boolean ={
+       (lst1,lst2) match {
+         case (Nil,_) => false
+         case (Nil,Nil) => true
+         case (_,Nil) => true
+         case (hd1::tail1,hd2::tail2) => if (hd1==hd2) {cont(tail1,tail2,count=1)}
+         else if(hd1 != hd2 && count ==1) return false
+         else cont(tail1,lst2,count)
+       }
+     }
+  
+     //-------------------------------------------------------------------------------------------------------     
+
+   val test: Tree =  NodeTree(7, NodeTree(5, EmptyTree(), NodeTree(8, EmptyTree(), EmptyTree())), EmptyTree())
+   val ls: List[Int] = List(1,2,3,4,5)
+   println("Tree: "+stringify(test))
+   //println("Tree: "+ MapTree(test,x=>x*3))
+   //println("Sum: "+ Sum(test))
+   //println("Ins: "+ InsTree(test,4))
+   //println("Fold: "+ FoldTree(0,test,(x,y)=>x+y))
+   println("Count: "+ cont(ls,List(2,4),0))
+   }
+  
+}
